db9a2279bf6d10ed6bbc4c7cfa3328699f4b8d63
JorcamAzofeifa <jazofeifa30294@ufide.ac.cr>
2026-03-23 23:04:05 -0600

    Update sistemaReservaGym.java

--- sistemaReservaGym.java
+++ sistemaReservaGym.java
@@ -1,163 +1,36 @@
+import javax.swing.JOptionPane;
+
 public class SistemaReservasGym {
-    
-    private static Cliente[] clientes = new Cliente[100];
-    private static ClaseGym[] clases = new ClaseGym[100];
-    private static Reserva[] reservas = new Reserva[100];
-    
-    private static int cantidadClientes = 0;
-    private static int cantidadClases = 0;
-    private static int cantidadReservas = 0;
 
     public static void main(String[] args) {
         menuPrincipal();
     }
 
     public static void menuPrincipal() {
-        int opcion = 0;
+        int opcion;
+
         do {
-            String menu = "MENU PRINCIPAL\n"
-                        + "1 Gestion de cliente\n"
-                        + "2 Gestion de clases\n"
-                        + "3 Gestion de reservas\n"
-                        + "4 Salir";
-            
-            String entrada = JOptionPane.showInputDialog(null, menu);
-            
-            if (entrada == null) {
-                opcion = 4;
+            String menu = "=== MENÚ PRINCIPAL ===\n"
+                    + "1. Gestión de Clientes\n"
+                    + "2. Gestión de Clases\n"
+                    + "3. Gestión de Reservas\n"
+                    + "4. Salir\n\n"
+                    + "Seleccione una opción:";
+
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+            if (opcion == 1) {
+                MenuClientes.menuClientes();
+            } else if (opcion == 2) {
+                MenuClases.menuClases();
+            } else if (opcion == 3) {
+                MenuReservas.menuReservas();
+            } else if (opcion == 4) {
+                JOptionPane.showMessageDialog(null, "Saliendo del sistema...");
             } else {
-                try {
-                    opcion = Integer.parseInt(entrada);
-                } catch (NumberFormatException e) {
-                    opcion = 0;
-                }
+                JOptionPane.showMessageDialog(null, "Opción inválida.");
             }
 
-            switch (opcion) {
-                case 1:
-                    menuClientes();
-                    break;
-                case 2:
-                    menuClases();
-                    break;
-                case 3:
-                    menuReservas();
-                    break;
-                case 4:
-                    JOptionPane.showMessageDialog(null, "Saliendo del sistema");
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Opcion invalida");
-            }
-        } while (opcion != 4);
-    }
-
-    public static void menuClientes() {
-        int opcion = 0;
-        do {
-            String menu = "GESTION DE CLIENTES\n"
-                        + "1 Registrar cliente\n"
-                        + "2 Listar clientes\n"
-                        + "3 Volver";
-            
-            String entrada = JOptionPane.showInputDialog(null, menu);
-            
-            if (entrada == null) {
-                opcion = 3;
-            } else {
-                try {
-                    opcion = Integer.parseInt(entrada);
-                } catch (NumberFormatException e) {
-                    opcion = 0;
-                }
-            }
-
-            switch (opcion) {
-                case 1:
-                    JOptionPane.showMessageDialog(null, "Opcion Registrar cliente en construccion");
-                    break;
-                case 2:
-                    JOptionPane.showMessageDialog(null, "Opcion Listar clientes en construccion");
-                    break;
-                case 3:
-                    break; 
-                default:
-                    JOptionPane.showMessageDialog(null, "Opcion invalida");
-            }
-        } while (opcion != 3);
-    }
-
-    public static void menuClases() {
-        int opcion = 0;
-        do {
-            String menu = "GESTION DE CLASES\n"
-                        + "1 Crear clase\n"
-                        + "2 Listar clases\n"
-                        + "3 Volver";
-            
-            String entrada = JOptionPane.showInputDialog(null, menu);
-            
-            if (entrada == null) {
-                opcion = 3;
-            } else {
-                try {
-                    opcion = Integer.parseInt(entrada);
-                } catch (NumberFormatException e) {
-                    opcion = 0;
-                }
-            }
-
-            switch (opcion) {
-                case 1:
-                    JOptionPane.showMessageDialog(null, "Opcion Crear clase en construccion");
-                    break;
-                case 2:
-                    JOptionPane.showMessageDialog(null, "Opcion Listar clases en construccion");
-                    break;
-                case 3:
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Opcion invalida");
-            }
-        } while (opcion != 3);
-    }
-
-    public static void menuReservas() {
-        int opcion = 0;
-        do {
-            String menu = "GESTION DE RESERVAS\n"
-                        + "1 Realizar reserva\n"
-                        + "2 Cancelar reserva\n"
-                        + "3 Listar reservas\n"
-                        + "4 Volver";
-            
-            String entrada = JOptionPane.showInputDialog(null, menu);
-            
-            if (entrada == null) {
-                opcion = 4;
-            } else {
-                try {
-                    opcion = Integer.parseInt(entrada);
-                } catch (NumberFormatException e) {
-                    opcion = 0;
-                }
-            }
-
-            switch (opcion) {
-                case 1:
-                    JOptionPane.showMessageDialog(null, "Opcion Realizar reserva en construccion");
-                    break;
-                case 2:
-                    JOptionPane.showMessageDialog(null, "Opcion Cancelar reserva en construccion");
-                    break;
-                case 3:
-                    JOptionPane.showMessageDialog(null, "Opcion Listar reservas en construccion");
-                    break;
-                case 4:
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Opcion invalida");
-            }
         } while (opcion != 4);
     }
 }
